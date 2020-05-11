7f5e8366674005314e71bf73738c9181fc372bdd
wjdcrw <51734038+wjdcrw@users.noreply.github.com>
2020-05-11 17:24:18 +0800

    first commit

---
+++ common/src/main/java/cn/dcr/proj/vo/ErrorType.java
@@ -0,0 +1,17 @@
+package cn.dcr.proj.vo;
+
+public interface ErrorType {
+    /**
+     * 返回code
+     *
+     * @return
+     */
+    String getStatus();
+
+    /**
+     * 返回mesg
+     *
+     * @return
+     */
+    String getMsg();
+}
---
+++ coustomlb-order/src/main/java/cn/dcr/proj/CoustomlbOrderApplication.java
@@ -0,0 +1,13 @@
+package cn.dcr.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoustomlbOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoustomlbOrderApplication.class, args);
+    }
+
+}
